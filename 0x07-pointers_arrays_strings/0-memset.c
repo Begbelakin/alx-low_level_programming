dc7a004ae1682cca060619f9673b481d39b7ea47
begbelakin <begbelakin@gmail.com>
2022-11-03 11:53:18 -0700

    pointers array

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h" 
+  
+ /** 
+  * _memset - fill memory with constant bytes 
+  * @s: memory region 
+  * @b: constant bytes 
+  * @n: count 
+  * 
+  * Return: a pointer to the memory area, NULL otherwise 
+  */ 
+  
+ char 
+ *_memset(char *s, char b, unsigned int n) 
+ { 
+         unsigned int c = 0; 
+  
+         while (c < n) 
+                 *(s + c++) = b; 
+  
+         return (s); 
+ }
